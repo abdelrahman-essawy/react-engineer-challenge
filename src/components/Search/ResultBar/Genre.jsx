9109476116700a3f926f83bcb5a6be6c4a3376be
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-01-14 10:02:19 +0200

    adding context to genre

--- src/components/Search/ResultBar/Genre.jsx
+++ src/components/Search/ResultBar/Genre.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
+import { useMoviesContext } from '../../../context/hooks/useMoviesContext';
 import { useToggleContext } from '../../../context/hooks/useToggleContext';
 import Border from '../../utilities/Border';
 
 export default function Genre() {
-  const { state, dispatch } = useToggleContext();
+  const { state: toggleState, dispatch: toggleDispatch } = useToggleContext();
+  const { state: moviesState, dispatch: moviesDispatch } = useMoviesContext();
+  const { movies } = moviesState;
+
   return (
     <>
       <div></div>
-      <div className={`col-span-1 ${state.genre ? `block` : `hidden`}`}>
+      <div className={`col-span-1 ${toggleState.genre ? `block` : `hidden`}`}>
         <Border>
           <div className='flex flex-col gap-2'>
             {
-              [...Array(11)].map((_, i) => (
-                <div className="flex flex-row items-center gap-3 md:gap-2 lg:gap-4">
-                  <input type="checkbox" className="w-4 h-4" />
-                  <span className="flex-1">sad</span>
+              movies.map(({ genre, id }) => (
+                <div
+                  key={id}
+                  className="flex flex-row items-center gap-3 md:gap-2 lg:gap-4">
+                  <input type="checkbox" className="w-4 h-4 cursor-pointer" />
+                  <span className="flex-1">{genre}</span>
                 </div>
               ))
             }
--- src/components/Search/ResultBar/Rating.jsx
+++ src/components/Search/ResultBar/Rating.jsx
@@ -14,7 +14,7 @@ export default function Rating() {
                     {
                         [...Array(11)].map((_, i) => (
                             <div className="flex flex-row items-center gap-3 md:gap-2 lg:gap-4">
-                                <input type="checkbox" className="w-4 h-4" />
+                                <input type="checkbox" className="w-4 h-4 cursor-pointer" />
                                 <span className="flex-1"><Stars rating={(i - 1) + 1} /></span>
                             </div>
                         ))
