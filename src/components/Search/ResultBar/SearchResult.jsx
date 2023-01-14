dd3217711a7bf6d2bfdcdf10af462bf1e1a51585
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-01-14 11:34:42 +0200

    adding functionality to searchresult

--- src/components/Search/ResultBar/SearchResult.jsx
+++ src/components/Search/ResultBar/SearchResult.jsx
@@ -1,28 +1,32 @@
 import React, { useEffect, useState } from 'react'
-import { useMoviesContext } from '../../../context/hooks/useMoviesContext';
+import { useSearchFilterContext } from '../../../context/hooks/useSearchFilterContext';
 import Border from '../../utilities/Border';
 import Stars from '../../utilities/Stars';
 
-export default function SearchResult({ movies, state}) {
+export default function SearchResult({ movies, state }) {
 
+  const { state: searchFilterState } = useSearchFilterContext();
 
+  const { keyword } = searchFilterState;
   return (
     <div>
-      <div className={`col-span-1 ${state ? `block` : `hidden`}`}>
+      <div className={`col-span-1 ${keyword ? `block` : `hidden`}`}>
         <Border>
           <div className="flex flex-col gap-4">
             {
-              movies.map(({ title, genre, id, rating }) => (
-                <div
-                  key={id}
-                  className="grid grid-cols-2 gap-1 items-start">
-                  <span className="sm:text-sm md:text-lg font-medium text-gray-900">{title}</span>
-                  <span className="sm:text-sm md:text-lg text-gray-600 place-self-end">{genre}</span>
-                  <span className='pr-4'>
-                    <Stars rating={rating} />
-                  </span>
-                </div>
-              ))
+              movies
+                .filter(({ title }) => title.toLowerCase().includes(keyword.toLowerCase()))
+                .map(({ title, genre, id, rating }) => (
+                  <div
+                    key={id}
+                    className="grid grid-cols-2 gap-1 items-start">
+                    <span className="sm:text-sm md:text-lg font-medium text-gray-900">{title}</span>
+                    <span className="sm:text-sm md:text-lg text-gray-600 place-self-end">{genre}</span>
+                    <span className='pr-4'>
+                      <Stars rating={rating} />
+                    </span>
+                  </div>
+                ))
             }
           </div>
 
