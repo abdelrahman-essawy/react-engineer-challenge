e26ebbac43f229bdfab2f383b78697997363cec9
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-01-15 03:57:49 +0200

    adding empty div for design inprove in genre

--- src/components/Search/ResultBar/Genre.jsx
+++ src/components/Search/ResultBar/Genre.jsx
@@ -14,27 +14,30 @@ const Genre = () => {
   const uniqueGenres = ['Any genre', ...new Set(movies.map(({ genre }) => genre))]
 
   return (
-    <div className={`col-span-1 ${toggleState.genre ? `block` : `hidden`}`}>
-      <StyledBorder>
-        <div className='flex flex-col gap-2'>
-          {
-            uniqueGenres
-              .map((genre, id) => (
-                <div key={id} className="flex flex-row items-center gap-3 md:gap-2 lg:gap-4">
-                  <input
-                    name='Genres'
-                    id={`Genre ${id}`}
-                    onChange={() => {
-                      searchFilterDispatch({ type: 'SET_GENRE', payload: genre });
-                    }}
-                    type="radio" className="w-4 h-4 cursor-pointer accent-black" value={genre} defaultChecked={genre === 'Any genre' ? true : false} />
-                  <label htmlFor={`Genre ${id}`} className="flex-1 cursor-pointer">{genre}</label>
-                </div>
-              ))
-          }
-        </div>
-      </StyledBorder>
-    </div>
+    <>
+      <div></div>
+      <div className={`col-span-1 ${toggleState.genre ? `block` : `hidden`}`}>
+        <StyledBorder>
+          <div className='flex flex-col gap-2'>
+            {
+              uniqueGenres
+                .map((genre, id) => (
+                  <div key={id} className="flex flex-row items-center gap-3 md:gap-2 lg:gap-4">
+                    <input
+                      name='Genres'
+                      id={`Genre ${id}`}
+                      onChange={() => {
+                        searchFilterDispatch({ type: 'SET_GENRE', payload: genre });
+                      }}
+                      type="radio" className="w-4 h-4 cursor-pointer accent-black" value={genre} defaultChecked={genre === 'Any genre' ? true : false} />
+                    <label htmlFor={`Genre ${id}`} className="flex-1 cursor-pointer">{genre}</label>
+                  </div>
+                ))
+            }
+          </div>
+        </StyledBorder>
+      </div>
+    </>
   );
 }
 
